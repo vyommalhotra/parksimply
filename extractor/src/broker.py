ce6cd9e477b0db820563e6618fcfc6b9f27500af
vyommalhotra <vyommalhotra@hotmail.com>
2020-11-13 20:20:22 -0500

    new src folder with rough comments

---
+++ extractor/src/broker.py
@@ -0,0 +1,25 @@
+if __name__ == "__main__":
+
+    # while there is still footage left
+    while (True):
+
+        # TODO: VYOM
+        # get frame from image
+        # calibrate fram skipping
+
+        # TODO: SARTHAK
+        # initialize spot class with image
+        # run spot detection on spot class
+        # get spot Ids from spot class
+
+        # TODO: SUNITH
+        # initialize car class with image
+        # run car detection on car class
+        # get car Ids from car class
+
+        # TODO: VYOM
+        # initilize matcher class with IDs of spots and cars
+        # For the IDs that don't yet have a match, perform a match
+        # for each previous and new match, get Image from car and spot classes
+        # save each image -> rest call to front end (later).
+        
